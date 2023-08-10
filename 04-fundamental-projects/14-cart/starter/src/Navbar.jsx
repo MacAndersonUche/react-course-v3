bfd9fb194e1558c1b854cd7e9c602053f9b52491
MacAnderson Uche <macandersonuche@yahoo.com>
2023-08-11 00:47:38 +0100

    fixed bug

--- 04-fundamental-projects/14-cart/starter/src/Navbar.jsx
+++ 04-fundamental-projects/14-cart/starter/src/Navbar.jsx
@@ -1,13 +1,29 @@
-import { FaCartPlus } from 'react-icons/fa';
+import { FaCartPlus } from "react-icons/fa";
+import { useEffect, useState } from "react";
+
+import { useGlobalContext } from "./context";
 const Navbar = () => {
+  const { state } = useGlobalContext();
+  const [qty, setQTY] = useState(0);
+
+  useEffect(() => {
+    let val = 0;
+
+    state.cart.forEach((item) => {
+      val += Number(item.amount);
+    });
+
+    setQTY(val);
+  }, [state]);
+
   return (
     <nav>
-      <div className='nav-center'>
+      <div className="nav-center">
         <h4>useReducer</h4>
-        <div className='nav-container'>
-          <FaCartPlus className='cart-icon' />
-          <div className='amount-container'>
-            <p className='total-amount'>2</p>
+        <div className="nav-container">
+          <FaCartPlus className="cart-icon" />
+          <div className="amount-container">
+            <p className="total-amount">{qty}</p>
           </div>
         </div>
       </div>
